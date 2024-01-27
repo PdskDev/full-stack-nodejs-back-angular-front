29702ca54236459236036df70d0ff3d6d6d7a006
NadetDev <nadetdev@gmail.com>
2024-01-27 21:48:12 +0100

    Add angular material + 3 main components

--- angular-crud-tutorial/src/app/app.module.ts
+++ angular-crud-tutorial/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddBookComponent } from './components/add-book/add-book.component';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
+import { BooksListComponent } from './components/books-list/books-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddBookComponent,
+    BookDetailsComponent,
+    BooksListComponent
   ],
   imports: [
     BrowserModule,
