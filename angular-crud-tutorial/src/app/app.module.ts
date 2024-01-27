f459c4d6335d624ede12cec420ea85aba7032599
NadetDev <nadetdev@gmail.com>
2024-01-27 21:53:38 +0100

    Remove non usable module

--- angular-crud-tutorial/src/app/app.module.ts
+++ angular-crud-tutorial/src/app/app.module.ts
@@ -1,24 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { AddBookComponent } from './components/add-book/add-book.component';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BookDetailsComponent } from './components/book-details/book-details.component';
 import { BooksListComponent } from './components/books-list/books-list.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddBookComponent,
     BookDetailsComponent,
-    BooksListComponent
+    BooksListComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
